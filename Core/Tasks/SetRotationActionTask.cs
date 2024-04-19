cda6a482ad11afb412f66147aff9f6e21236f89a
KimScor <58765606+kimjisoo4@users.noreply.github.com>
2024-04-19 20:35:33 +0900

    Add Variable
    [ Added ]
     SerializableReference 용 Variable 추가.

---
+++ Core/Tasks/SetRotationActionTask.cs
@@ -0,0 +1,71 @@
+﻿using StudioScor.Utilities;
+using System;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using static UnityEngine.UI.GridLayoutGroup;
+
+namespace StudioScor.RotationSystem
+{
+    [Serializable]
+    public class SetRotationActionTask : ActionTask, ISubActionTask
+    {
+        [Header(" [ Ser Rotation Action Task ] ")]
+        [SerializeField] private bool _isUpdatable = false;
+        [SerializeReference]
+#if SCOR_ENABLE_SERIALIZEREFERENCE
+        [SerializeReferenceDropdown]
+#endif
+        private IDirectionVariable _direction = new LocalDirectionVariable(Vector3.forward);
+
+        public bool IsFixedUpdate => false;
+
+        private IRotationSystem _rotationSystem;
+
+        private SetRotationActionTask _original;
+
+        protected override void SetupTask()
+        {
+            base.SetupTask();
+
+            _rotationSystem = Owner.GetRotationSystem();
+
+            _direction.Setup(Owner);
+        }
+
+        public override IActionTask Clone()
+        {
+            var clone = new SetRotationActionTask();
+
+            clone._original = this;
+            clone._direction = _direction.Clone() as IDirectionVariable;
+
+            return clone;
+        }
+
+        protected override void EnterTask()
+        {
+            base.EnterTask();
+
+            Vector3 direction = _direction.GetValue();
+
+            Quaternion rotation = Quaternion.LookRotation(direction, Vector3.up);
+
+            _rotationSystem.SetRotation(rotation.eulerAngles, true);
+        }
+
+        public void UpdateSubTask(float normalizedTime)
+        {
+            var isUpdatable = _original is null ? _isUpdatable : _original._isUpdatable;
+
+            if (!isUpdatable)
+                return;
+
+            Vector3 direction = _direction.GetValue();
+
+            _rotationSystem.SetRotation(direction);
+
+        }
+
+        
+    }
+}
